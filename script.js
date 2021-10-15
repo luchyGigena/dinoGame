e9a2b965295b3f5f6badd8b69482d25168cd2d58
gigenaucian <73915741+gigenaucian@users.noreply.github.com>
2021-10-15 13:51:16 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const player = document.getElementById("player");
+
+
+document.addEventListener("click", function(){
+    player.classList.add("playerJump")
+} );
+
+player.addEventListener("animationend", ()=>{
+    player.classList.remove('playerJump');
+    console.log("player end")
+});
