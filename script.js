a74f5e82238198a1ae70f88a80e1418843b6dc21
gigenaucian <73915741+gigenaucian@users.noreply.github.com>
2021-10-19 12:11:33 -0300

    function pause, continue

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const player = document.getElementById("player");
-
+const cactus = document.getElementById("cactus");
+const background = document.getElementById("background")
+const buttonPlayStop = document.getElementById("buttonPlayStop");
+let score= 0;
 
 document.addEventListener("click", function(){
     player.classList.add("playerJump")
@@ -9,3 +12,25 @@ player.addEventListener("animationend", ()=>{
     player.classList.remove('playerJump');
     console.log("player end")
 });
+function stopAnimation(){
+    cactus.style.animationPlayState ='paused';
+    player.style.animationPlayState ='paused';
+    background.style.animationPlayState='paused';
+
+}
+
+buttonPlayStop.addEventListener('click', () => {
+    buttonPlayStop.classList.toggle("play");
+    stopAnimation();
+})
+
+
+
+
+
+
+setInterval(()=>{
+    score++;
+    document.getElementById("score").innerText =score
+    
+},1000)
